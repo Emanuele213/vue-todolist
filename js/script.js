f57276b6b2f2ce2ec44b2b999d321bf5a656b99e
Emanuele213 <vittorius213@gmail.com>
2022-11-15 19:59:51 +0100

    complete exercise without text bar on click

---
+++ js/script.js
@@ -0,0 +1,35 @@
+new Vue({
+    el:'#app',
+    data:{
+        arrTodos:[
+            {
+                text:'Fai i compiti',
+                done: false,
+            },
+            {
+                text:'Fai un pisolino',
+                done: true,
+            },
+            {
+                text:'Guada un film',
+                done: true,
+            }
+        ],
+        newWord: '',
+    },
+    methods:{
+        addWord() {
+            if(this.newWord === 0) return;
+            this.arrTodos.push({
+                text: this.newWord,
+                done: this.newWord, 
+            })
+            console.log(this.newWord);
+            this.newWord = '';
+            
+        },
+        deleteWord(index) {
+            this.arrTodos.splice(index, 1);
+        },
+    }
+}); 
